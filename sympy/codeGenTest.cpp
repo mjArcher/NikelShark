3d503a1bc300e39d15c8e7a118f86e72bbcc2c22
Matthew Archer <matt.james.archer@gmail.com>
2016-05-10 22:07:23 +0100

    experimenting with code generation

---
+++ sympy/codeGenTest.cpp
@@ -0,0 +1,25 @@
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+
+/* #include <string> */
+using namespace std;
+
+double result;
+
+extern "C" 
+{
+  #include "./ots22/romenskii_energy.inc"
+}
+
+int main(int argc, char ** argv)
+{
+
+}
+
+
+
+
